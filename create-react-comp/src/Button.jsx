a431b4138b78fbc08d15fde5c63d19e3b936e6f7
Monika Kushwaha <monikakushwaha2001@gmail.com>
2024-06-10 23:52:21 +0530

    added some comp.. to learn more about comp.

---
+++ create-react-comp/src/Button.jsx
@@ -0,0 +1,6 @@
+function Button(){
+    return(
+        <button>Like the video</button>
+    )
+}
+export default Button;
---
+++ create-react-comp/src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello(){
+    return(
+        <>
+        <h3>Hello this the hello function.</h3>
+        <p>To understand component i have created more than one component.</p>
+        </>
+    )
+}
+export default Hello;
